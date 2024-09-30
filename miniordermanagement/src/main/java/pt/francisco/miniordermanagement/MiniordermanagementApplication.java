9ccfe3018bcd67ece910989104d64a54ca3dd710
francisco.barbosa <francisco.barbosa@kuehne-nagel.com>
2024-09-30 22:35:04 +0100

    initial commit

---
+++ miniordermanagement/src/main/java/pt/francisco/miniordermanagement/MiniordermanagementApplication.java
@@ -0,0 +1,13 @@
+package pt.francisco.miniordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniordermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniordermanagementApplication.class, args);
+	}
+
+}
---
+++ miniordermanagement/src/test/java/pt/francisco/miniordermanagement/MiniordermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.francisco.miniordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniordermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
